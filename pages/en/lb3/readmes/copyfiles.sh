28211e39f7eb38e966bbe0d6846c56790c935dce
Rand McKinney <crandmck@yahoo.com>
2017-01-10 17:10:48 -0800

    Move doc into connector READMEs (#236)

---
+++ pages/en/lb3/readmes/copyfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp loopback-connector-mssql.md ../../../../../loopback-connector-mssql/README.md
+cp loopback-connector-postgresql.md ../../../../../loopback-connector-postgresql/README.md
+cp loopback-connector-oracle.md ../../../../../loopback-connector-oracle/README.md
+cp loopback-connector-redis.md ../../../../../loopback-connector-redis/README.md
+cp loopback-connector-remote.md ../../../../../loopback-connector-remote/README.md
+cp loopback-connector-rest.md ../../../../../loopback-connector-rest/README.md
