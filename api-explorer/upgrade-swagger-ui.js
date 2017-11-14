2400ae5b585c73d6dc478a5c794d210bb9b91293
Raymond Feng <enjoyjava@gmail.com>
2017-11-14 14:50:30 -0800

    Add a hosted swagger UI for LoopBack apps (#530)

---
+++ api-explorer/upgrade-swagger-ui.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback.io
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+
+async function upgradeSwaggerUI() {
+  const sourceDir = path.dirname(
+    require.resolve('swagger-ui-dist/package.json')
+  );
+  const pkg = require('swagger-ui-dist/package.json');
+  const swaggerUIModule = `swagger-ui-dist@${pkg.version}`;
+  
+  console.log('Swagger UI %s found in %s.', swaggerUIModule, sourceDir);
+
+  const targetDir = __dirname;
+
+  const filesToKeep = [
+    'README.md',
+    'package.json',
+    '.npmignore',
+    'index.loopback.html',
+    'upgrade-swagger-ui.js'
+  ];
+
+  const filterFunc = (src, dest) => {
+    const file = path.basename(src);
+    return filesToKeep.indexOf(file) === -1;
+  };
+
+  const files = await fs.readdir(targetDir);
+  const filesToRemove = files.filter(f => filesToKeep.indexOf(f) === -1);
+
+  for (const f of filesToRemove) {
+    await fs.remove(path.join(targetDir, f));
+  }
+
+  await fs.copy(sourceDir, targetDir, { filter: filterFunc });
+
+  // Overlay index.html
+  await fs.copy(
+    path.join(targetDir, 'index.loopback.html'),
+    path.join(targetDir, 'index.html')
+  );
+
+  console.log(`API Explorer has been upgrades to swagger-ui-dist@${pkg.version}.`);
+  console.log();
+  console.log(`Please create a github pull request against gh-pages branch with changes in ${targetDir}.`);
+}
+
+upgradeSwaggerUI();
