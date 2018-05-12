bf98bc63327aa7742d5ecb039e63c2f5e37324e8
Taranveer Virk <taranveer@virk.cc>
2018-05-12 13:32:25 -0400

    feat: discovery powered search and sitemap gen

---
+++ sitemap.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const xml2js = require('xml2js');
+
+// Read sitemap.xml
+const sitemap = fs.readFileSync('./_site/sitemap.xml', {encoding:'utf-8'});
+
+// Convert XML to JSON
+xml2js.parseString(sitemap, function(err, json) {
+  // Create a list of URLs -- replacing localhost with loopback.io
+  const lines = [];
+  json.urlset.url.forEach(url => {
+    let link = url.loc[0];
+    lines.push(link);
+  });
+
+  // Write SiteMap as JSON for other tools
+  fs.writeJSONSync('_site/sitemap.json', json);
+  
+  // Write txt file of list of all links
+  fs.writeFileSync('_site/sitemap.txt', lines.join('\n'));
+});
