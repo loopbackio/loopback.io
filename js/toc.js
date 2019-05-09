11006b59c0443e45294e07d850cde113e3035ff2
Agnes Lin <agneslin.lin@ibm.com>
2019-05-09 08:43:42 -0400

    chore: update copyrights years

--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback.io-workflow-scripts
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 // https://github.com/ghiculescu/jekyll-table-of-contents
 (function($){
   $.fn.toc = function(options) {
